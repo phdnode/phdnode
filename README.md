7b674588d70f54a23aef7d67f2eac7a322a942ba
Sholto Maud <sholto.maud@gmail.com>
2015-10-07 16:35:25 +1000

    secodn

---
+++ README.md
@@ -0,0 +1,15 @@
+# phdnode
+
+Some Node.js CLI scripts and ideas on how to manage & generate documents relating to PhD generation
+
+Follows bionode
+
+## Requireify
+
+Generate a requirements document out of a Literature Review
+
+* need to specify the md format required for the scraper to work
+
+## Abstractify
+
+Pull together chapter abstracts for a confirmation document 
---
+++ cli.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+var minimist = require('minimist')
+var debug = require('debug')
+var spawn = require('child_process').spawn
+var colors = require('./colors')
+var components = require('./components')
+
+var phd = require('./')
+var spawn = require('child_process').spawn
+var minimist = require('minimist')
+
+
+var minimistOptions = {
+  alias: {
+    help: 'h'
+  }
+}
+
+var argv = minimist(process.argv.slice(2), minimistOptions)
+
+if (argv.help || argv._.length === 0) {
+  console.log("Please check the documentation at http://doc.bionode.io")
+}
+
+var component = argv._[0]
+var path = __dirname + '/node_modules/bionode-' + component + '/cli.js'
+var args = argv._.slice(1)
+var cli = spawn(path, args)
+
+cli.stdout.pipe(process.stdout)
+cli.stderr.pipe(process.stderr)
+
+if (argv._[argv._.length - 1] === '-') {
+  process.stdin.pipe(cli.stdin)
+} else {
+  cli.stdin.end()
+}
